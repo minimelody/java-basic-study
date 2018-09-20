bfbae04518f350142a6f623f9383367c280ae52c
minimelody <minimelodi@naver.com>
2018-09-20 15:28:01 +0900

    변수 선언하고 값 대입하기

---
+++ chapter02/src/chapter2/Variable1.java
@@ -0,0 +1,12 @@
+package chapter2;
+
+public class Variable1 {
+
+	public static void main(String[] args) {
+		int age;		//정수형 변수 level을 선언
+		age = 27;		//level 변수에 값 10을 대입
+		System.out.println(age);	//level 값 출력
+
+	}
+
+}
